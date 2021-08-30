516488f43949f4d4339c2cb554f946c740e56ff4
SumitKachareSDP <sumit@sheelldesae.com>
2021-08-30 10:36:09 +0530

    cors added

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 const userRoutes = require("./route");
+
+
 
 const PORT = process.env.PORT || 5000
 
+// enable cors
+app.use(cors());
+app.options("*", cors());
+
 // parse json request body
 app.use(express.json());
+
+app.use(cors());
 
 app.use("/api", userRoutes)
 
